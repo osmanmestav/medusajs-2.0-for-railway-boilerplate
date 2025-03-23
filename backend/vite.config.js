5d5d012a77b8003eb088fcccea3bcfff9cb23110
osmanmestav <osmanmestav@hotmail.com>
2025-03-23 11:34:25 +0300

    update vite config

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 7000,
-    strictPort: true,
-    allowedHosts: ["fearless-unity-production.up.railway.app"],
-    cors: {
-      origin: ["https://fearless-unity-production.up.railway.app"],
-      credentials: true
-    }
-  }
+    server: {
+        host: "0.0.0.0",
+        port: 7000,
+        strictPort: true,
+        allowedHosts: ["http://localhost:7000", "fearless-unity-production.up.railway.app", "https://fearless-unity-production.up.railway.app"],
+        cors: {
+            origin: ["http://localhost:7000", "fearless-unity-production.up.railway.app", "https://fearless-unity-production.up.railway.app"],
+            credentials: true
+        }
+    },
+    preview: {
+        port: 7000,
+        strictPort: true,
+        host: '0.0.0.0',
+        allowedHosts: true,
+    },
 });
