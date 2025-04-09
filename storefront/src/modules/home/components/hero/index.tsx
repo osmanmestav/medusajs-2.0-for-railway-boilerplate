9892e716e843b25c913c18c1d1c29f530a0e8728
osmanmestav <osmanmestav@hotmail.com>
2025-04-09 14:57:37 +0300

    updata stype problem

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -6,7 +6,11 @@ const Hero = () => {
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div
         className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6"
-        style="background: url('https://images.unsplash.com/photo-1726065235239-b20b88d43eea?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-size: cover; background-position: center;"
+        style={{
+          background: "url('https://images.unsplash.com/photo-1726065235239-b20b88d43eea?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
         <span>
           <Heading
