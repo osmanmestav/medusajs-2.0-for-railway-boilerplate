a6bd02ca4e72823ca74abfe8398de2509ae3a97a
osmanmestav <osmanmestav@hotmail.com>
2025-04-09 14:52:29 +0300

    update banner

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -4,7 +4,10 @@ import { Button, Heading } from "@medusajs/ui"
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+      <div
+        className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6"
+        style="background: url('https://images.unsplash.com/photo-1726065235239-b20b88d43eea?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-size: cover; background-position: center;"
+      >
         <span>
           <Heading
             level="h1"
@@ -23,9 +26,7 @@ const Hero = () => {
           href="https://funkyton.com/medusajs-2-0-is-finally-here/"
           target="_blank"
         >
-          <h1 style={{ textDecoration: "underline" }}>
-            Visit the tutorial
-          </h1>
+          <h1 style={{ textDecoration: "underline" }}>Visit the tutorial</h1>
         </a>
       </div>
     </div>
